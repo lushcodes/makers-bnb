43ca2bd890940166fca6b6aee75aaee2d834b498
William Young <wadyoung@outlook.com>
2021-07-27 11:31:20 +0100

    Deleted coverage and .gitignore

--- db/migrations/list_space.sql
+++ db/migrations/list_space.sql
@@ -1,4 +1,4 @@
-1. CREATE DATABASE bnb;
+1. CREATE DATABASE space;
 2. Connect
 3. CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(64), description VARCHAR(500), price VARCHAR(8));
 4. * Repeat for a test database
