e58bf37707c9ac4379d82333835ba25f4431e415
William Young <wadyoung@outlook.com>
2021-07-29 14:47:55 +0100

    Partial intergration

--- lib/space.rb
+++ lib/space.rb
@@ -2,28 +2,30 @@
 
 require 'pg'
 require './app'
+require_relative 'user'
 
 class Space
   attr_reader :name, :description, :price, :space_id
 
   # user_id:, available? to be added
-  def initialize(space_id:, name:, description:, price:)
+  def initialize(space_id:, name:, description:, price:, user_id:)
+    @user_id = user_id
     @space_id = space_id
     @name = name
     @description = description
     @price = price
   end
 
-  def self.create(name:, description:, price:)
+  def self.create(name:, description:, price:, user_id:)
     conn = if ENV['RACK_ENV'] == 'test'
              PG.connect(dbname: 'bnb_test')
            else
              PG.connect(dbname: 'bnb')
            end
 
-    result = conn.exec("INSERT INTO spaces (name, description, price)
-    VALUES('#{name}', '#{description}', '#{price}') RETURNING space_id, name, description, price;")
-    Space.new(space_id: result[0]['space_id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'])
+    result = conn.exec("INSERT INTO spaces (name, description, price, owner_user_id)
+    VALUES('#{name}', '#{description}', '#{price}', '#{user_id}') RETURNING owner_user_id, space_id, name, description, price;")
+    Space.new(space_id: result[0]['space_id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], user_id: result[0]['owner_user_id'])
   end
 
   def self.find_space(id:)
@@ -33,7 +35,7 @@ class Space
         PG.connect(dbname: 'bnb')
       end
     result = conn.exec("SELECT * FROM spaces WHERE space_id = '#{id}';")
-    Space.new(space_id: result[0]['space_id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'])
+    Space.new(space_id: result[0]['space_id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], user_id: result[0]['owner_user_id'])
   end
 
   def self.list
@@ -45,7 +47,7 @@ class Space
 
     result = conn.exec('SELECT * FROM spaces;')
     result.map do |space|
-      Space.new(space_id: result[0]['space_id'], name: space['name'], description: space['description'], price: space['price'])
+      Space.new(space_id: result[0]['space_id'], name: space['name'], description: space['description'], price: space['price'], user_id: result[0]['owner_user_id'])
     end
   end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'space'
+require 'user'
 
 describe Space do
   describe '.create' do
@@ -16,14 +17,20 @@ describe Space do
 
   describe '.list' do
     it 'displays all spaces' do
+      user = User.create(username: 'Luke', email: 'lukerocks@party.io', password: 'Flumpy')
+     
+      p 'output'
+     p user
+      p user.id
       Space.create(name: 'Arthurs retreat',
                    description: 'A beautiful lakeside house, with a view of a legendary sword',
-                   price: '10,000')
-
+                   price: '10,000',
+                   user_id: user.id)
       spaces = Space.list
       expect(spaces.first.name).to eq 'Arthurs retreat'
       expect(spaces.first.description).to eq 'A beautiful lakeside house, with a view of a legendary sword'
       expect(spaces.first.price).to eq '10,000'
+      expect(spaces.first.owner_user_id).to eq user.id
     end
 
     describe '.find' do
